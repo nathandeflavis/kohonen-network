09d0affe5f5c11d0459ecd450aad482ef6122269
Nathan De Flavis <92447278+nathandeflavis@users.noreply.github.com>
2023-09-25 15:23:30 +0100

    Update __main__.py
    Added docstrings to module and functions.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,15 @@
+"""
+A module that is the entry point into the program,
+responsible for getting user input
+and passing data points to the 'kohonen' module.
+"""
 from kohonen import *
 from vector import *
 from csv import *
 
+"""
+A function that is the entry point into the program.
+"""
 def main():
     setToUse = input("Use <Training> or <Test> set? ")
     dataPointsToNormalise = get_data_points("Sets/" + setToUse + ".csv")
@@ -23,6 +31,9 @@ def main():
     
     cluster(dataPointsNotToNormalise, units)
 
+"""
+A function to extract data points from a data file with a given path.
+"""
 def get_data_points(dataFilePath):
     dataPoints = []
     
@@ -38,7 +49,10 @@ def get_data_points(dataFilePath):
     file.close()
     return dataPoints
 
-#sample data point
+"""
+A function to generate Kohonen units
+with the dimensionality of a given data point.
+"""
 def get_units(dataPoint):
     units = []
 
