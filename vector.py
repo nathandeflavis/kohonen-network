6a71e8a97123e9bc2f8f0e8112e428061c9f0041
Nathan De Flavis <92447278+nathandeflavis@users.noreply.github.com>
2023-09-25 15:24:56 +0100

    Update vector.py
    Added docstrings to class and functions.

--- vector.py
+++ vector.py
@@ -1,9 +1,20 @@
+"""
+A class representing an n-dimensional vector,
+responsible for computing its length,
+and normalising and performing arithmetic on it.
+"""
 from math import *
 
 class Vector:
+    """
+    A function to initialise this vector with given coordinates.
+    """
     def __init__(self, coordinates):
         self._coordinates = coordinates
 
+    """
+    A function to represent this vector as a string.
+    """
     def __str__(self):
         s = "(" + str(self._coordinates[0])
 
@@ -13,9 +24,16 @@ class Vector:
         s += ")"
         return s       
 
+    """
+    A getter function for this vector's coordinates.
+    """
     def get_coordinates(self):
         return self._coordinates
-    
+
+    """
+    A function to get this vector's length
+    as the square root of the sum of its squared coordinates.
+    """
     def get_length(self):
         squareSum = 0.0
 
@@ -24,6 +42,10 @@ class Vector:
 
         return sqrt(squareSum)
 
+    """
+    A function to normalise this vector
+    such that its coordinates are in the interval [0, 1].
+    """
     def normalise(self):
         length = self.get_length()
         coordinates = self._coordinates
@@ -31,12 +53,20 @@ class Vector:
         for i in range(0, len(coordinates)):
             coordinates[i] /= length
 
+    """
+    A function to add the coordinates of a given other vector
+    to the coordinates of this vector.
+    
+    """
     def add(self, vector):
         myCoordinates = self._coordinates
         
         for i in range(0, len(myCoordinates)):
             myCoordinates[i] += vector._coordinates[i]
 
+    """
+    A function to multiply this vector's coordinates by a given multiplier.
+    """
     def multiply_by(self, multiplier):
         coordinates = self._coordinates
         
