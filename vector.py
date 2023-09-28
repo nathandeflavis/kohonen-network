02d49e540c2efce4859f01c9a3c0bdb67e2de61c
Nathan De Flavis <92447278+nathandeflavis@users.noreply.github.com>
2023-09-28 01:29:01 +0100

    Update vector.py
    Added tests.

--- vector.py
+++ vector.py
@@ -12,20 +12,49 @@ class Vector:
     def __init__(self, coordinates):
         self._coordinates = coordinates
 
+    def test_init():
+        x = y = z = 1
+        expected_coordinates = [x, y, z]
+        vector = Vector(expected_coordinates)
+        actual_coordinates = vector.get_coordinates()
+        start = 0
+        stop = len(expected_coordinates)
+
+        for i in range(start, stop):
+            expected_coordinate = expected_coordinates[i]
+            actual_coordinate = actual_coordinates[i]
+            assert actual_coordinate == expected_coordinate
+
     """
     A function to represent this vector as a string.
     """
     def __str__(self):
         coordinates = self._coordinates
-        coordinateStrings = []
+        coordinate_strings = []
 
         for coordinate in coordinates:
-            coordinateString = str(coordinate)
-            coordinateStrings.append(coordinateString)
+            coordinate_string = str(coordinate)
+            coordinate_strings.append(coordinate_string)
         
         delimiter = ", "
-        s = "(" + delimiter.join(coordinateStrings) + ")"
+        s = "(" + delimiter.join(coordinate_strings) + ")"
         return s
+
+    def test_str():
+        x = y = z = 1
+        coordinates = [x, y, z]
+        coordinate_strings = []
+
+        for coordinate in coordinates:
+            coordinate_string = str(coordinate)
+            coordinate_strings.append(coordinate_string)
+        
+        delimiter = ", "
+        expected = "(" + delimiter.join(coordinate_strings) + ")"
+
+        vector = Vector(coordinates)
+        actual = str(vector)
+        assert actual == expected
 
     """
     A getter function for this vector's coordinates.
@@ -39,16 +68,26 @@ class Vector:
     as the square root of the sum of its squared coordinates.
     """
     def get_length(self):
-        squareSum = 0.0
+        square_sum = 0.0
         coordinates = self._coordinates
         power = 2
 
         for coordinate in coordinates:
             square = coordinate ** power
-            squareSum += square
+            square_sum += square
 
-        length = sqrt(squareSum)
+        length = sqrt(square_sum)
         return length
+
+    def test_get_length():
+        x = 0
+        y = 3
+        z = 4
+        coordinates = [x, y, z]
+        vector = Vector(coordinates)
+        expected = 5
+        actual = vector.get_length()
+        assert actual == expected
 
     """
     A function to normalise this vector
@@ -58,28 +97,63 @@ class Vector:
         length = self.get_length()
         coordinates = self._coordinates
         start = 0
-        dimensionality = len(coordinates)
+        stop = len(coordinates)
 
-        for i in range(start, dimensionality):
+        for i in range(start, stop):
             coordinate = coordinates[i]
             coordinate /= length
             coordinates[i] = coordinate
+
+    def test_normalise():
+        x = 0
+        y = 3
+        z = 4
+        coordinates = [x, y, z]
+        vector = Vector(coordinates)
+        vector.normalise()
+        expected_coordinates = []
+        length = vector.get_length()
+
+        for coordinate in coordinates:
+            expected_coordinate = coordinate / length
+            expected_coordinates.append(expected_coordinate)
+
+        actual_coordinates = vector.get_coordinates()
+        start = 0
+        stop = len(expected_coordinates)
+
+        for i in range(start, stop):
+            expected_coordinate = expected_coordinates[i]
+            actual_coordinate = actual_coordinates[i]
+            assert actual_coordinate == expected_coordinate
+        
 
     """
     A function to add the coordinates of that given vector
     to the coordinates of this vector.
     """
     def add(self, vector):
-        thisCoordinates = self._coordinates
-        thatCoordinates = vector._coordinates
+        this_coordinates = self._coordinates
+        that_coordinates = vector._coordinates
         start = 0
-        thisDimensionality = len(thisCoordinates)
+        stop = len(this_coordinates)
         
-        for i in range(start, thisDimensionality):
-            thisCoordinate = thisCoordinates[i]
-            thatCoordinate = thatCoordinates[i]
-            thisCoordinate += thatCoordinate
-            thisCoordinates[i] = thisCoordinate
+        for i in range(start, stop):
+            this_coordinate = this_coordinates[i]
+            that_coordinate = that_coordinates[i]
+            this_coordinate += that_coordinate
+            this_coordinates[i] = this_coordinate
+
+    def test_add():
+        x = y = z = 1
+        coordinates = [x, y, z]
+        vector = Vector(coordinates)
+        vector.add(vector)
+        coordinates = vector.get_coordinates()
+        expected = 2
+
+        for coordinate in coordinates:
+            assert coordinate == expected
 
     """
     A function to multiply this vector's coordinates by a given multiplier.
@@ -87,9 +161,20 @@ class Vector:
     def multiply_by(self, multiplier):
         coordinates = self._coordinates
         start = 0
-        dimensionality = len(coordinates)
+        stop = len(coordinates)
         
-        for i in range(start, dimensionality):
+        for i in range(start, stop):
             coordinate = coordinates[i]
             coordinate *= multiplier
             coordinates[i] = coordinate
+
+    def test_multiply_by():
+        x = y = z = 1
+        coordinates = [x, y, z]
+        vector = Vector(coordinates)
+        multiplier = 2
+        vector.multiply_by(multiplier)
+        coordinates = vector.get_coordinates()
+
+        for coordinate in coordinates:
+            assert coordinate == multiplier
